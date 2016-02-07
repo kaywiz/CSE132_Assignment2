b18785b29847698ddb9b5ef72bb7ab9e8adc4ee7
k.wisneski <k.wisneski@ACCOUNTS.AD.WUSTL.EDU>
2016-02-07 06:32:13 +0000

    finished assignment2

--- indicator/indicator.ino
+++ indicator/indicator.ino
@@ -4,8 +4,8 @@
  *  
  *  Fill this out so we know whose assignment this is.
  *  
- *  Name:
- *  WUSTL Key:
+ *  Name: Kelly Wisneski
+ *  WUSTL Key: k.wisneski
  *  
  *  and if two are partnered together
  *  
@@ -13,13 +13,49 @@
  *  WUSTL Key:
  */
 
-
+#define analogPin A0
+#define red 11
+#define green 9
+#define blue 10
+int val = 0;
+int val1 = 0;
+int redVal = 0;
+int greenVal = 0;
+int blueVal = 0;
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  val1 = analogRead(analogPin);
+  val = analogRead(analogPin);
+  if (val1 < 682){
+    val = (val1 * 3) / 8; //converts to 255 scale
+    redVal = 0;
+    greenVal = val;
+    blueVal = 255 - val;
+    //Serial.print(greenVal);
+    //Serial.println(blueVal);
+  }
+ /* else if (val1 < 682){
+    val = ((val1 - 341) * 3) / 8;
+    redVal = 0;
+    blueVal = 255 - val;
+    greenVal = val;
+  }*/
+  else{
+    greenVal = 0;
+    blueVal = 0;
+    digitalWrite(red, 255);
+    delay(1000);
+    digitalWrite(red, 0);
+    delay(1000);
+  }
+  analogWrite(green, greenVal);
+  analogWrite(blue, blueVal);
+  analogWrite(red, redVal);
+  Serial.println(val1);
+  delay(50);
 }
